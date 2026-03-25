de9374e86416165f9b6ab4f5bed41b9cb0e852b4
Kamsi Rodrick <148686197+RodrickKamsiyonna@users.noreply.github.com>
2026-03-25 18:20:16 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'mapbox-gl': 'mapbox-gl',
+    };
+
+    // Required for mapbox-gl to work with Next.js
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
