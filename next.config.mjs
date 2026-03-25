5752d1f97d98214b02ad285e5d467a20c84e4480
Kamsi Rodrick <148686197+RodrickKamsiyonna@users.noreply.github.com>
2026-03-25 18:21:12 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
+  turbopack: {
+    resolveAlias: {
       'mapbox-gl': 'mapbox-gl',
-    };
-
-    // Required for mapbox-gl to work with Next.js
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-    };
-
-    return config;
+    },
   },
 };
 
